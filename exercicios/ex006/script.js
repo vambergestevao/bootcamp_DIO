9febfeb7c868da9dc3da944c05d083e05db3a8bc
vambergestevao <vambergfreitas@gmail.com>
2024-05-03 18:11:11 -0300

    Criei ex006 à ex008

---
+++ exercicios/ex006/script.js
@@ -0,0 +1,8 @@
+// Guardando coisas em Vetores (sempre começa por "0")
+let nomesPokemon = ["Pikachu" , "Charmander", "Bulbasaur"]
+
+//funções ou métodos
+pokemon.pop() // remove the last element
+pokemon.shift() // remove the first element
+
+console.log(pokemon)
---
+++ exercicios/ex008/script.js
@@ -0,0 +1,10 @@
+//Como usar Vetores e Matrizes
+let nomesPokemon = ("Pikachu", "Charmander", "Bulbasaur")
+let timePokemon = [
+ //col 0 , col 1 , col 2
+    ["Pikachu", "M", 1] //linha 0
+    ["chamander", "F", 3] //linha 1
+]
+
+//propriedade
+console.log(timePokemon.length)
