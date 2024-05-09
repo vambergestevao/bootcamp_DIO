09c04f8badeef6bf58fa8cd21250e0bc310a6110
vambergestevao <vambergfreitas@gmail.com>
2024-05-09 20:55:05 -0300

    Criei ex025

---
+++ exercicios/ex025/index.js
@@ -0,0 +1,51 @@
+// Função
+torrar()
+
+function torrar() {
+    console.log("torrando pão")
+    injetarPao()
+}
+
+function injetarPao() {
+    console.log("preparando para injetar o pão")
+    console.log("finalizado")
+}
+
+
+// Função
+main()
+
+function main() {
+    getData()
+    checkValues()
+    sendToDatabase()
+}
+
+function getData() {
+// lógica implementada aqui
+    console.log("pegando dados do usuário")
+    if (1 < 3) {
+        console.log("numero encontrado")
+    }
+}
+
+function checkValues() {
+    console.log("validando dados")
+}
+
+function sendToDatabase() {
+    console.log("cadastrando dados")
+}
+
+
+// Função
+enviarDados()
+
+function enviarDados() {
+    let nomeDoBanco = "banco-de-dados"
+    console.log("salvando dados em: " + nomeDoBanco)
+
+    if (3 === 3) {
+        console.log("senha validada")
+    }
+}
